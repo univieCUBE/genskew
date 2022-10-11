00911d7d8d5ee94a0e44560eb9f9297e84806654
Julius Krämer <87365694+JulleeeK@users.noreply.github.com>
2022-10-11 08:31:20 +0200

    Add files via upload
    added build files

---
+++ GUIskew_0.1.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GUIskew",
+    version="0.1.2",
+    author="Thomas Rattei, Julius Krämer",
+    url='https://genskew.csb.univie.ac.at/guiskew',
+    description="A program that calculates genskew.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="Creative Commons",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+          'matplotlib',
+          'Genskew_univiecube',
+          'Genskew_cc',
+      ],
+)
